97f89b03b2ec7a3977459f90b641316415d8becc
memonbayu11 <you@example.com>
2020-10-01 13:17:21 +0700

    myintentapp2

---
+++ app/src/main/java/com/dicoding/picodiploma/myintentapp/MoveActivity.kt
@@ -0,0 +1,14 @@
+package com.dicoding.picodiploma.myintentapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+
+}
